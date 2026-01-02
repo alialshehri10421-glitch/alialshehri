9d552bfc51351f4393492844396283747d7117c8
alialshehri10421-glitch <ali.alshehri.1042.1@gmail.com>
2026-01-02 16:27:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,24 @@
-gpa = input("what type of GPA do you want? (scientific/literary/tvtc) ")
+import streamlit as st
 
-school = int(input("Your cumulative GPA in high school: "))
-gat = int(input("Your score in the GAT (Qudurat): "))
-saat = int(input("Your score in the SAAT (Tahseeli): "))
+st.title("حساب المعدل التراكمي")
 
-if gpa == "scientific" :
-    average = (school*3 + gat*3 + saat*4 ) / 10
-    
-if gpa == "literary" :
-    average = (school*5 + gat*5 ) / 10
-    
-if gpa == "tvtc" :
-    average = (school*5 + gat*3 + saat*2 ) / 10
-    
-print(average)
+نوع = st.selectbox(
+    "اختر نوع المسار",
+    ["علمي", "ادبي", "الكلية التقنية"]
+)
+
+school = st.number_input("معدل الثانوية", min_value=0.0, max_value=100.0)
+gat = st.number_input("درجة اختبار القدرات", min_value=0.0, max_value=100.0)
+saat = st.number_input("درجة اختبار التحصيلي", min_value=0.0, max_value=100.0)
+
+if st.button("احسب المعدل"):
+    if نوع == "علمي":
+        average = (school*3 + gat*3 + saat*4) / 10
+
+    elif نوع == "ادبي":
+        average = (school*5 + gat*5) / 10
+
+    elif نوع == "الكلية التقنية":
+        average = (school*5 + gat*3 + saat*2) / 10
+
+    st.success(" المعدل التراكمي = {average}")
